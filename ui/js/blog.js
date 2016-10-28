72e7161023fd65bf3e80d123e9b40f42791538e1
Harsh Shah <harshshah1295@gmail.com>
2016-10-28 22:38:15 +0530

    [imad-console] Updates ui/js/blog.js

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -1,4 +1,21 @@
 function getBlog(x){
-    alert(x);
+        var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();          
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/$x', true);
+    request.send(null);
 }
 
