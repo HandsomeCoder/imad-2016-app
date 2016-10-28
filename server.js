6309881082c7e1c37d9f0b72048898fb417c7f3c
Harsh Shah <harshshah1295@gmail.com>
2016-10-28 21:49:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ function createBlog(){
                 <title>Blog</title>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link href="ui/site.css" rel="stylesheet"/>
-                <link href="ui/blog.css" rel="stylesheet"/>
+                <link href="ui/css/site.css" rel="stylesheet"/>
+                <link href="ui/css/blog.css" rel="stylesheet"/>
             </head>
             <body>
                 <nav>
