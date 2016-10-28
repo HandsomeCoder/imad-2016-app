ec09fa91d954f5063ec028b4205e5849e8dd0c26
Harsh Shah <harshshah1295@gmail.com>
2016-10-28 12:56:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,10 +16,22 @@ app.get('/ui/css/style.css', function (req, res) {
 app.get('/ui/css/site.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/css', 'site.css'));
 });
+
+app.get('/ui/css/profile.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/css', 'profile.css'));
+});
+
+app.get('/ui/html/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/html', 'profile.html'));
+});
+
 app.get('/ui/images/ME_0.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui/images', 'ME_0.jpg'));
 });
 
+app.get('/ui/images/ME_2.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/images', 'ME_2.jpg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
