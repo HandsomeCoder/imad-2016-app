d60ce2095179be1abb5d7a1c2fa10c1bfd03bdef
Harsh Shah <harshshah1295@gmail.com>
2016-10-28 12:51:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/css/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.sendFile(path.join(__dirname, 'ui/css', 'style.css'));
 });
 
 app.get('/ui/ME_1.jpg', function (req, res) {
