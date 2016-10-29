cb77512b89fa54e02e40bbf20b6c6fda22fe5fbc
Harsh Shah <harshshah1295@gmail.com>
2016-10-29 19:36:13 +0530

    [imad-console] Updates ui/js/blog.js

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -1,7 +1,6 @@
 function getBlog(x){
         var request = new XMLHttpRequest();
     
-    // Capture the response and store it in a variable
     request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           // Take some action
@@ -10,11 +9,22 @@ function getBlog(x){
                 document.getElementById('displayReg').innerHTML = blogContent.toString();   
           }
       }  
-      // Not done yet
     };
     
-    // Make the request
     request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/'+x, true);
     request.send(null);
 }
 
+function getTitle(){
+    request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+                var blogContent = request.responseText;
+                document.getElementById('displayReg').innerHTML = blogContent.toString();   
+          }
+      }  
+    };
+    
+    request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/title', true);
+    request.send(null);
+}
