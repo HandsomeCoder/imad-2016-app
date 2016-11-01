70e0159ab9ecb02fee4be2c918d843cb8c02af5b
Harsh Shah <harshshah1295@gmail.com>
2016-11-01 14:45:17 +0530

    [imad-console] Updates ui/js/blog.js

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -40,12 +40,27 @@ function addComment(){
     request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
- 
+                loadComment(blogId);
           }
       }  
     };
     
     
     request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/comment?blogId='+blogId+'&name='+name+'&comment='+comment, true);
+    request.send(null);
+}
+
+function loadComment(blogId){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+                var commentContent = request.responseText;
+                document.getElementById('').innerHTML = blogTitle.toString();   
+          }
+      }  
+    };
+    
+    request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/fetchComment?blogId'+blogId, true);
     request.send(null);
 }
