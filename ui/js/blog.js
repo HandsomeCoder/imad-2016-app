6cd67cdd455be314c1eff0e7602e49bea65a6035
Harsh Shah <harshshah1295@gmail.com>
2016-11-01 17:31:55 +0530

    [imad-console] Updates ui/js/blog.js

--- ui/js/blog.js
+++ ui/js/blog.js
@@ -42,6 +42,8 @@ function addComment(){
       if (request.readyState === XMLHttpRequest.DONE) {
           if (request.status === 200) {
                 loadComment(blogId);
+                document.getElementById('name').innerHTML = "";
+                document.getElementById('comment').innerHTML = "";
           }
       }  
     };
