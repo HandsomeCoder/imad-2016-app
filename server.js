db3870d5be080bc67d6e1596bcbeccc099172f92
Harsh Shah <harshshah1295@gmail.com>
2016-11-01 21:22:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -215,7 +215,7 @@ function createBlogContent(data){
                     <div>
                         <input type="text" id="name" placeholder="Enter your name"/>
                         <textarea id="comment" placeholder="Enter your Comment" rows="4"></textarea><br>
-                        <input type="submit" value="Submit" onclick="addComment()"/>
+                        <input class="float-right" type="submit" value="Submit" onclick="addComment()"/>
                     </div>
                 </section>`;
     return blogTemp;
