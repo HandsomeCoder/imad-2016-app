2d69a0e00a3e381c0785d04718f06908e939ad1a
Harsh Shah <harshshah1295@gmail.com>
2016-11-01 17:29:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.get('/blog/q/comment', function (req, res) {
     var name = req.query.name;
     var comment = req.query.comment;
     console.log(blogId);
-    pool.query('INSERT INTO "commentRecord" ("blogid", "name", "comment") VALUES ($1,$2,$3)',[blogId,name,comment],function(err,res){
+    pool.query('INSERT INTO "commentRecord" ("blogid", "name", "comment") VALUES ($1,$2,$3)',[blogId,name,comment],function(err,rows){
         if(err){
             console.log("fail");
         }
