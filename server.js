7d3879bd2711654a71a2abc8d2569f9dee53423c
Harsh Shah <harshshah1295@gmail.com>
2016-11-02 17:40:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-//var wait=require('wait.for');
-//var sleep = require('sleep');
 
 var config = {
     user: '',
