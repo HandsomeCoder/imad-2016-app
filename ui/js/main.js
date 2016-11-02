8fdf98c4633f5ea5a6d39f34820f365cadef1d4f
Harsh Shah <harshshah1295@gmail.com>
2016-11-02 16:55:06 +0530

    [imad-console] Updates ui/js/main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,1 +1,36 @@
+function addComment(){
+    var blogId = document.getElementById('blogId').innerHTML;
+    blogId = parseInt(blogId);
+    var name = document.getElementById('name').value;
+    var comment = document.getElementById('comment').value;
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+                loadComment(blogId);
+                document.getElementById('name').value = "";
+                document.getElementById('comment').value = "";
+          }
+      }  
+    };
+    
+    
+    request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/comment?blogId='+blogId+'&name='+name+'&comment='+comment, true);
+    request.send(null);
+}
 
+function loadComment(blogId){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+                var commentContent = request.responseText;
+                document.getElementById('postedComment').innerHTML = commentContent.toString();   
+          }
+      }  
+    };
+    
+    request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/fetchComment?blogId='+blogId, true);
+    request.send(null);
+}
