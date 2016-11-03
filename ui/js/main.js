12ca0a1119fbaa1256f14174c14a9ed23b9fde32
Harsh Shah <harshshah1295@gmail.com>
2016-11-03 10:29:59 +0530

    [imad-console] Updates ui/js/main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -2,48 +2,57 @@ function check(){
     var name = document.getElementById('name');
     var comment = document.getElementById('comment');
     if(name.value === ""){
-        name.getElementById('name').classList.add("invalid");
+        name.classList.add("invalid");
+        return false;
     }
     if(comment.value === ""){
-        comment.classList.add = "invalid";
+        comment.classList.add("invalid");
+        return false;
+    }
+    else{
+        name.classList.remove("invalid");
+        comment.classList.remove("invalid");
+        return true;
     }
 }
 
 
 function addComment(x){
-    var request = new XMLHttpRequest();
-    var name = document.getElementById('name').value;
-    var comment = document.getElementById('comment').value;
-    
-    if(x == 'b'){
-        var blogId = document.getElementById('blogId').innerHTML;
-        blogId = parseInt(blogId);
+    if(check()){
+        var request = new XMLHttpRequest();
+        var name = document.getElementById('name').value;
+        var comment = document.getElementById('comment').value;
         
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              if (request.status === 200) {
-                    loadComment(blogId,null);
-                    document.getElementById('name').value = "";
-                    document.getElementById('comment').value = "";
-              }
-          }  
-        };
-        request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/comment?blogId='+blogId+'&name='+name+'&comment='+comment, true);
-        request.send(null);
-    }
-    else{
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-              if (request.status === 200) {
-                    loadComment(null,true);
-                    document.getElementById('name').value = "";
-                    document.getElementById('comment').value = "";
-              }
-          }  
-        };
-        request.open('GET', 'http://handsomecoder.imad.hasura-app.io/profile/comment?name='+name+'&comment='+comment, true);
-        request.send(null);
-    
+        if(x == 'b'){
+            var blogId = document.getElementById('blogId').innerHTML;
+            blogId = parseInt(blogId);
+            
+            request.onreadystatechange = function () {
+              if (request.readyState === XMLHttpRequest.DONE) {
+                  if (request.status === 200) {
+                        loadComment(blogId,null);
+                        document.getElementById('name').value = "";
+                        document.getElementById('comment').value = "";
+                  }
+              }  
+            };
+            request.open('GET', 'http://handsomecoder.imad.hasura-app.io/blog/q/comment?blogId='+blogId+'&name='+name+'&comment='+comment, true);
+            request.send(null);
+        }
+        else{
+            request.onreadystatechange = function () {
+              if (request.readyState === XMLHttpRequest.DONE) {
+                  if (request.status === 200) {
+                        loadComment(null,true);
+                        document.getElementById('name').value = "";
+                        document.getElementById('comment').value = "";
+                  }
+              }  
+            };
+            request.open('GET', 'http://handsomecoder.imad.hasura-app.io/profile/comment?name='+name+'&comment='+comment, true);
+            request.send(null);
+        
+        }
     }
 }
 
