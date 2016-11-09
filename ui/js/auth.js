346f093a152803f09dff55c610737c9503353fee
Harsh Shah <harshshah1295@gmail.com>
2016-11-09 16:58:17 +0530

    Update and rename auth.js to signup.js

--- ui/js/auth.js
+++
@@ -1,7 +0,0 @@
-module.export = {
-    'facebookAuth' :{
-        'clientID' : '347580915597066',
-        'clientSecret' : '1b8d07059807fdb9ebbe1463c716edc1',
-        'callbackURL' : 'http://handsomecoder.imad.hasura-app.io/auth/facebook/callback'
-    }
-};
