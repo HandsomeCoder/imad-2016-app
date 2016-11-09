1d5f503d606f0e6372caeadf7fa5e4126df0da45
Harsh Shah <harshshah1295@gmail.com>
2016-11-09 17:04:55 +0530

    [imad-console] Updates ui/js/signup.js

--- ui/js/signup.js
+++ ui/js/signup.js
@@ -1,1 +1,37 @@
-
+var submit = document.getElementById('submit');
+submit.onclick = function () {
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var uname = document.getElementById('uname').value;
+        var fname = document.getElementById('fname').value;
+        var lname = document.getElementById('lname').value;
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({uname: uname,fname: fname,lname: lname,email: email, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
