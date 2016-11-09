2e4fe9a6fc4f6237aa2e2915defe0eee3bc99eae
Harsh Shah <harshshah1295@gmail.com>
2016-11-09 19:27:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -218,7 +218,7 @@ app.post('/signup/user', function (req, res) {
     });
 });
 
-app.get('/signin/check', function (req, res) {
+app.post('/signin/check', function (req, res) {
     var uname = req.body.uname;
     var password = req.body.password;
     pool.query('SELECT * FROM "ceredentail" WHERE uname = $1', [uname], function (err, result) {
