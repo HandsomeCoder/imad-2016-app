1cfdf9983866e15dfeb99991bc6dddca5e79d531
Harsh Shah <harshshah1295@gmail.com>
2016-11-09 12:07:08 +0530

    [imad-console] Updates ui/js/auth.js

--- ui/js/auth.js
+++ ui/js/auth.js
@@ -2,6 +2,6 @@ module.export = {
     'facebookAuth' :{
         'clientID' : '347580915597066',
         'clientSecret' : '1b8d07059807fdb9ebbe1463c716edc1',
-        'callbackURL' : 'http://handsomecoder.imad.hasura-app.io/auth/facebook'
+        'callbackURL' : 'http://handsomecoder.imad.hasura-app.io/auth/facebook/callback'
     }
 };
