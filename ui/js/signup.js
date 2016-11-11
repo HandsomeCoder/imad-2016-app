42f669542ee6ff1db9820d54ad437a6553be0661
Harsh Shah <harshshah1295@gmail.com>
2016-11-11 18:41:12 +0530

    [imad-console] Updates ui/js/signup.js

--- ui/js/signup.js
+++ ui/js/signup.js
@@ -7,12 +7,12 @@ signup.onclick = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  signup.value = 'Sucess!';
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                  signup.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  signup.value = 'Login';
               } else {
                   alert('Something went wrong on the server');
                   submit.value = 'Login';
@@ -31,7 +31,7 @@ signup.onclick = function () {
         request.open('POST', 'http://handsomecoder.imad.hasura-app.io/signup/user', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({fname: fname,lname: lname,email: email, password: password}));  
-        submit.value = 'Logging in...';
+        signup.value = 'Logging in...';
         
     };
     
@@ -44,12 +44,12 @@ signin.onclick = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
-                  submit.value = 'Sucess!';
+                  signin.value = 'Sucess!';
               } else if (request.status === 403) {
-                  submit.value = 'Invalid credentials. Try again?';
+                  signin.value = 'Invalid credentials. Try again?';
               } else if (request.status === 500) {
                   alert('Something went wrong on the server');
-                  submit.value = 'Login';
+                  signin.value = 'Login';
               } else {
                   alert('Something went wrong on the server');
                   submit.value = 'Login';
@@ -66,6 +66,6 @@ signin.onclick = function () {
         request.open('POST', 'http://handsomecoder.imad.hasura-app.io/signin/check', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({email: email,password: password}));  
-        submit.value = 'Logging in...';
+        signin.value = 'Logging in...';
         
     };
