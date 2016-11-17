480c7d8b476d17e5c733ccac5d7f7d3b9a903847
Harsh Shah <harshshah1295@gmail.com>
2016-11-17 17:55:53 +0530

    [imad-console] Updates ui/js/sign.js

--- ui/js/sign.js
+++ ui/js/sign.js
@@ -39,7 +39,7 @@ function validateonsignup(){
         check = false;
     }
     if(password.value !== "" && cpassword.value !== ""){
-        if(password.value.length >= 4 || password.value.length <= 15){
+        if(password.value.length <= 4 || password.value.length >= 15){
             password.classList.add("invalid");
             cpassword.classList.add("invalid");
             alert("Password length should be between 4 to 15 characters");
