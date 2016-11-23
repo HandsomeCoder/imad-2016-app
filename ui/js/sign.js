aeb0ba44c00297b389182833de9f6fb0415446b1
Harsh Shah <harshshah1295@gmail.com>
2016-11-23 14:27:50 +0530

    Update sign.js

--- ui/js/sign.js
+++ ui/js/sign.js
@@ -98,6 +98,10 @@ function validateonsignin(){
 
 var signup = document.getElementById('signupBtn');
 signup.onclick = function () {
+    var fname = document.getElementById('fname').value;
+    var lname = document.getElementById('lname').value;
+    var email = document.getElementById('emailup').value;
+    var password = document.getElementById('passwordup').value;
     if(validateonsignup() === true){
             var request = new XMLHttpRequest();
             request.onreadystatechange = function () {
@@ -110,7 +114,7 @@ signup.onclick = function () {
                       if(c === true){
                           var con = prompt("Enter \"CONFIRM\" to override the password"); 
                           if(con === "confirm" || con === "CONFIRM"){
-                                alert("Password Override! Try login with new password");
+                                updateUser(fname,lname,email,password);
                           }
                       }
                 } else if (request.status === 500) {
@@ -123,10 +127,7 @@ signup.onclick = function () {
                 }  
             };
         
-            var fname = document.getElementById('fname').value;
-            var lname = document.getElementById('lname').value;
-            var email = document.getElementById('emailup').value;
-            var password = document.getElementById('passwordup').value;
+
             console.log(fname);
             console.log(password);
             request.open('POST', 'http://handsomecoder.imad.hasura-app.io/signup/user', true);
@@ -134,7 +135,27 @@ signup.onclick = function () {
             request.send(JSON.stringify({fname: fname,lname: lname,email: email, password: password}));  
             signup.value = 'Logging in...';
         }
+};
+
+function updateUser(fname,lname,email,password){
+     var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+                alert("Password Override! Try login with new password");
+          }else{
+            alert("Something went wrong! Try Again");
+          }
+      }  
     };
+    
+    request.open('GET', 'http://handsomecoder.imad.hasura-app.io/signup/updateuser, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({fname: fname,lname: lname,email: email, password: password}));
+    request.send(null);
+}
     
 var signin = document.getElementById('signinBtn');
 signin.onclick = function () {
